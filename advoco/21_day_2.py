a06763776d7e07c07007d5ae05ac787d2b12c383
Ben Cook <bfc782@hotmail.com>
2021-12-02 06:43:49 +0100

    comment out test

--- advoco/21_day_2.py
+++ advoco/21_day_2.py
@@ -1,5 +1,5 @@
 # %%
-directions = []
+# directions = []
 horiz = 0
 aim = 0
 vert = 0
@@ -15,7 +15,7 @@ with open('21_day2_i.txt', 'r') as f:
                 aim = aim + int(dir[1])
             else: 
                 aim = aim - int(dir[1])
-        print(dir[0], dir[1], horiz, aim, vert)
+        # print(dir[0], dir[1], horiz, aim, vert)
 
 print(horiz, vert)
 print(horiz*vert)
