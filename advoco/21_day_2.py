999c2cf338940937d3dda6c746ad3d505e3d6c6b
Ben Cook <bfc782@hotmail.com>
2021-12-02 06:41:37 +0100

    submarine steering

---
+++ advoco/21_day_2.py
@@ -0,0 +1,23 @@
+# %%
+directions = []
+horiz = 0
+aim = 0
+vert = 0
+
+with open('21_day2_i.txt', 'r') as f:
+    for line in f:
+        dir = line.split()
+        if dir[0] == 'forward':
+            horiz = horiz + int(dir[1])
+            vert = vert + int(dir[1])*aim
+        else:
+            if dir[0] == 'down':
+                aim = aim + int(dir[1])
+            else: 
+                aim = aim - int(dir[1])
+        print(dir[0], dir[1], horiz, aim, vert)
+
+print(horiz, vert)
+print(horiz*vert)
+
+# %%
